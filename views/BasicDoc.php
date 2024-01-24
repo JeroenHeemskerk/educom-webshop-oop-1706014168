eab1f1320d4d9c8d2107cd505442bf19f689f660
patrick lubbers <rosevalley@gmail.com>
2024-01-24 11:31:32 +0100

    #01-B opdracht 4.1

---
+++ views/BasicDoc.php
@@ -0,0 +1,14 @@
+<?php
+
+  include_once "../views/HtmlDoc.php";
+
+class BasicDoc extends HtmlDoc {
+	
+	
+	
+}
+
+//die overerft van de HtmlDoc en de gemeenschappelijke zaken
+//neerzet zoals header, menu en footer.
+
+?>
---
+++ views/HtmlDoc.php
@@ -0,0 +1,14 @@
+<?php
+
+//Maak in de folder /views een class file HtmlDoc aan, die een kale HTML pagina kan genereren, 
+//met maar 1 publieke methode show() die alleen maar private en protected methoden aanroept.
+
+class HtmlDoc {
+	
+	
+	public function show() {
+		//call a bunch of private and protected methods
+	}
+}
+
+?>
