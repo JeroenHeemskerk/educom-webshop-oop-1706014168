0549a694e4270f98558e277be9adeb7f3c8771f7
patrick lubbers <rosevalley@gmail.com>
2024-01-25 10:02:06 +0100

    made extra test files

---
+++ tests/test_about_doc.php
@@ -0,0 +1,6 @@
+<?php
+  include_once "../views/AboutDoc.php";
+  
+  $view = new AboutDoc();
+  $view  -> show();
+?>
---
+++ tests/test_home_doc.php
@@ -0,0 +1,7 @@
+<?php
+  include_once "../views/HomeDoc.php";
+  
+  $view = new HomeDoc();
+  $view  -> show();
+?>
+
