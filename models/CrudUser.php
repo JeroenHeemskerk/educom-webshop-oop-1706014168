6979de0a27a0ef32417373cc995c5a4936ba7aff
patrick lubbers <rosevalley@gmail.com>
2024-02-05 18:35:47 +0100

    #10 set default value for  property in constructor of UserCrud class (username)

--- models/CrudUser.php
+++ models/CrudUser.php
@@ -4,21 +4,21 @@ require_once "Crud.php";
 
 class UserCrud {
     private $crud; //property storing instance of generic crud class
+    private $table;
 
-    function __construct(Crud $crud) { //use dependency injection
+    function __construct(Crud $crud, $table = "username") { //use dependency injection
         $this->crud = $crud;
+        $this->table = $table
     }
 
     function createUser($username, $password) { //use dependency injection, etc.
-        $table = "username";
-        $sql = "INSERT INTO $table (username, password_hashed) VALUES (?, ?)";
+        $sql = "INSERT INTO $this->table (username, password_hashed) VALUES (?, ?)";
         $params = [$username, $password];
         return $this->crud->createRow($sql, $params);
     }
 
     function readUserByUsername($username) {
-        $table = "username";
-        $sql = "SELECT * FROM $table WHERE username = ?";
+        $sql = "SELECT * FROM $this->table WHERE username = ?";
         $params = [$username];
         return $this->crud->readOneRow($sql, $params);
     }
@@ -28,8 +28,7 @@ class UserCrud {
     //operations related to retrieving userdata
 
     function retrieveUserData($column, $username) {
-        $table = "username";
-        $sql = "SELECT $column FROM $table WHERE username = ?";
+        $sql = "SELECT $column FROM $this->table WHERE username = ?";
         $params = [$username];
         $data = $this->crud->readOneRow($sql, $params);
         //this fetches an object. how to make it usable?
@@ -41,22 +40,19 @@ class UserCrud {
     }
 
     function getUserId($username) {
-        $table = "username";
-        $sql = "SELECT id From $table WHERE username = ?";
+        $sql = "SELECT id From $this->table WHERE username = ?";
         $params = [$username];
         $userId = $this->crud->readOneRow($sql, $params);
     }
 
     function updateUser($userId, $newUsername) {
-        $table = "username";
-        $sql = "UPDATE $table SET username = ? WHERE id = ?";
+        $sql = "UPDATE $this->table SET username = ? WHERE id = ?";
         $params = [$newUsername, $userId];
         return $this->crud->updateRow($sql, $params);
     }
 
     public function deleteUser($userId) {
-        $table = "username";
-        $sql = "DELETE FROM $table WHERE id = ?";
+        $sql = "DELETE FROM $this->table WHERE id = ?";
         $params = [$userId];
         return $this->crud->deleteRow($sql, $params);
     }
